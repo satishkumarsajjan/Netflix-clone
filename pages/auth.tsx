0c73c099d7c7d1f3ea565fd6b9eb5e765501b305
satishkumar <satishsajjan4564@gmail.com>
2023-03-20 15:09:36 +0530

    Mongodb, prisma, Next Auth setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,13 +1,14 @@
 import Input from '@/components/input';
 import React, { useState, useCallback } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { NextPageContext } from 'next';
-// import { getSession, signIn } from 'next-auth/react';
+import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -19,14 +20,41 @@ const Auth = () => {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/',
+      });
+
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className='bg-black w-full h-full lg:bg-opacity-50'>
+      <div className='bg-black w-full h-full sm:bg-opacity-50'>
         <nav className='px-12 py-5'>
           <img src='/images/logo.png' className='h-12' alt='Logo' />
         </nav>
         <div className='flex justify-center'>
-          <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+          <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 md:w-2/3 lg:w-2/5 lg:max-w-md rounded-md w-full sm:w-9/10'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>
               {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
@@ -56,7 +84,7 @@ const Auth = () => {
               />
             </div>
             <button
-              // onClick={variant === 'login' ? login : register}
+              onClick={variant === 'login' ? login : register}
               className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'
             >
               {variant === 'login' ? 'Login' : 'Sign up'}
